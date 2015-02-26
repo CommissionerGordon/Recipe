535fdd54b057b722f33ffbee50e3ca4daf6a4e23
Amanda Jaynes <AMAJAYNE@uat.edu>
2015-02-26 10:43:44 -0700

    WebScraper

---
+++ Recipe-WebScraper/Recipe-WebScraper/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recipe_WebScraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void urlTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //get text written in text box as an url
+            string url = urlTextBox.Text;
+            //use url to get the website's source code
+            string sourceCode = WebScraper.getSourceCode(url);
+            //write the sourceCode to a file
+            StreamWriter streamWrite = new StreamWriter("website.txt");
+            streamWrite.Write(sourceCode);
+            streamWrite.Close();
+        }
+    }
+}
---
+++ Recipe-WebScraper/Recipe-WebScraper/WebScraper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;   //allows us to make web requests
+using System.IO;
+
+namespace Recipe_WebScraper
+{
+    class WebScraper
+    {
+        public static string getSourceCode(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader source = new StreamReader(response.GetResponseStream());
+            string sourceCode = source.ReadToEnd();
+            source.Close();
+            response.Close();
+            string snipOfSourceCode = TextFollowing(sourceCode, "H1");
+            return snipOfSourceCode;
+        }
+
+        public static string TextFollowing(string searchText, string value) //allows for searching the text after a certain key word or phrase
+        {
+            if (!String.IsNullOrEmpty(searchText) && !String.IsNullOrEmpty(value))
+            {
+                int index = searchText.IndexOf(value);
+                if (-1 < index)
+                {
+                    int start = index + value.Length;
+                    if (start <= searchText.Length)
+                    {
+                        return searchText.Substring(start);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
+
